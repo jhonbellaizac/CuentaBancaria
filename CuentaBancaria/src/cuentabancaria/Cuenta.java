f485a254b7f0befb5f25dfe5862c256bc1770fff
jhonbellaizac <janerb123@gmail.com>
2025-03-14 20:38:21 -0500

    cree el metodo imprimir

--- CuentaBancaria/src/cuentabancaria/Cuenta.java
+++ CuentaBancaria/src/cuentabancaria/Cuenta.java
@@ -1,11 +1,33 @@
 package cuentabancaria;
 public class Cuenta {
-
 protected float saldo;
 protected int numConsignacion = 0;
 protected int numRetiro = 0;
 protected float tasaAnual;
 protected float comisionMensual = 0;
+
+public Cuenta (float saldo, float tasaAnual){
+    this.saldo = saldo;
+    this.tasaAnual = tasaAnual;
+}
+
+public void imprimir (){
+    System.out.println("Saldo: " + saldo);
+    System.out.println("Numero de consignacion: " + numConsignacion);
+    System.out.println("Numero de retiro: " + numRetiro);
+    System.out.println("Tasa anual: " + tasaAnual);
+    System.out.println("Comision mensul: " + comisionMensual);
+    
+}
+    
+
+
+    
+
+
+
+
+
 
 
 
