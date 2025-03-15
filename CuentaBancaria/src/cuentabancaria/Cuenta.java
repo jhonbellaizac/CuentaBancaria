a0b234157571cde8f53b743fc55b7fa798e4665e
jhonbellaizac <janerb123@gmail.com>
2025-03-14 20:29:28 -0500

    Cree atributos

---
+++ CuentaBancaria/src/cuentabancaria/Cuenta.java
@@ -0,0 +1,34 @@
+package cuentabancaria;
+public class Cuenta {
+
+protected float saldo;
+protected int numConsignacion = 0;
+protected int numRetiro = 0;
+protected float tasaAnual;
+protected float comisionMensual = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
