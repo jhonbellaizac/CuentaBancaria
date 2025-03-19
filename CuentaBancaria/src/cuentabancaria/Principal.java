e060c5b3beabeabe11e0ad1ad48417b7032de6d5
jhonbellaizac <janerb123@gmail.com>
2025-03-18 20:39:53 -0500

    agregue las clase cuenta de ahorro y cuenta corriente

--- CuentaBancaria/src/cuentabancaria/Principal.java
+++ CuentaBancaria/src/cuentabancaria/Principal.java
@@ -2,17 +2,41 @@ package cuentabancaria;
 public class Principal {
     public static void main(String[] args) {
         
-       Cuenta cuenta = new Cuenta (100000, 0); 
-       cuenta.imprimir();
+        System.out.println("Cuenta de Ahorro: ");
+        
+        final float  SALDO = 15000;
+        final float TASA_ANUAL =  0.10f;
+        
+       Cuenta cuentaAhorros1 = new Cuenta (SALDO, TASA_ANUAL); 
+       
         System.out.println("");
         
-       cuenta.consignar(56500);
-       cuenta.imprimir();
+        cuentaAhorros1.imprimir();
+        
         System.out.println("");
         
-       cuenta.retirar(25200);
-       cuenta.imprimir();
+        cuentaAhorros1.consignar(5000);
+        cuentaAhorros1.consignar(5000);
+        cuentaAhorros1.consignar(5000);
+        cuentaAhorros1.consignar(5000);
+        cuentaAhorros1.imprimir();
+        
         System.out.println("");
+        
+        cuentaAhorros1.retirar(2000);
+        cuentaAhorros1.retirar(200);
+        cuentaAhorros1.retirar(500);
+        cuentaAhorros1.retirar(100);
+        cuentaAhorros1.retirar(3000);
+        cuentaAhorros1.retirar(700);
+        cuentaAhorros1.retirar(800);
+        cuentaAhorros1.imprimir();
+        
+       
+     
+       
+        
+      
 
 
 
